0d1cb35a313c4a1aa5d432d9bad35e411658c3c7
Hyun <jjjtoy@naver.com>
2025-10-16 14:13:07 +0900

    chore: swagger 연결

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestiaSwaggerComposer } from '@nestia/sdk';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
+
+  const document = await NestiaSwaggerComposer.document(app, {
+    info: {
+      title: 'Test API 문서',
+      version: '1.0.0',
+    },
+    openapi: '3.1',
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Localhost',
+      },
+    ],
+  });
+  SwaggerModule.setup('api', app, document as any);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+main();
